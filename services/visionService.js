33cdf277efe14ceee71d0620be37091881d5588c
ktj1997 <rlaxowns7916@gmail.com>
2021-08-12 12:14:33 +0900

    fix:벡터값 동일하게 나오는 에러 수정, 필요없는 벡터 제거

--- services/visionService.js
+++ services/visionService.js
@@ -2,7 +2,6 @@ const tf = require('@tensorflow/tfjs-node');
 const posnet = require('@tensorflow-models/posenet');
 const {createCanvas,Image} = require('canvas');
 const path = require("path");
-const { json } = require('express');
 
 const imageScaleFactor = 0.5;
 const outputStride = 16;
@@ -15,20 +14,20 @@ exports.tryModel = async(imgList) => {
         outputStride: 16,
         inputResolution: 513,
         multiplier: 0.75});
-
-    const img = new Image();
     const canvas = createCanvas(500,500)
     const ctx = canvas.getContext('2d')    
-    img.onload = () => ctx.drawImage(img,500,500);
 
-    for await(imgName of imgList){
+    for await(const imgName of imgList){
+        const indexOutput={}
+        const img = new Image();
+        img.onload = () => ctx.drawImage(img,0,0);
         img.src = path.join(__dirname,`../images/${imgName}`)
         const input = tf.browser.fromPixels(canvas)
         const pose = await net.estimateSinglePose(input,imageScaleFactor,flipHorizontal,outputStride)
-        let indexOutput = {}
+        pose.keypoints.filter((keypoint,idx) => idx >= 5).forEach(keypoint => indexOutput[`${keypoint.part}`] ={x:keypoint.position.x,y:keypoint.position.y})
+        output.push(indexOutput);
         for await (const keypoint of pose.keypoints) 
             indexOutput[`${keypoint.part}`] ={x:keypoint.position.x,y:keypoint.position.y}
-        output.push(indexOutput);
     }
     return output
 }
