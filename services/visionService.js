17117bea826b59f195ac55f68651abb6e54f9cf8
ktj1997 <rlaxowns7916@gmail.com>
2021-08-24 18:30:58 +0900

    fix:model config

--- services/visionService.js
+++ services/visionService.js
@@ -1,26 +1,27 @@
 const tf = require('@tensorflow/tfjs-node');
 const posnet = require('@tensorflow-models/posenet');
-const {createCanvas,Image} = require('canvas');
+const {createCanvas,Image, Canvas} = require('canvas');
 const path = require("path");
 const {cutFrames,getImages,deleteImages} = require("../services/imageService")
 const { poseSimilarity} = require('posenet-similarity')
+const PNG = require("png.js")
 const uuid = require("../services/uuidUtil")
 
-const imageScaleFactor = 0.5;
-const outputStride = 16;
-const flipHorizontal = false;
 const  exclude = ["nose","leftEye","rightEye","leftEar","rightEar"]
+const width =1920, height=1080
 
 exports.getSimilarity = async(trainer, user) =>{
     const output = await this.getVector(user)
     let sum=0,length = trainer.length > output.length?output.length:trainer.length
-    
+    let sum2=0
     for (i=0;i<length;i++){
-        const similarity = poseSimilarity(trainer[i],output[i],{strategy:"cosineSimilarity"})
-        //const value = Math.log2(Math.pow(similarity+1,100))
-        sum += similarity
+        const similarity = poseSimilarity(trainer[i],output[i],{strategy:'weightedDistance'})
+        const value = Math.log2(Math.pow(similarity+1,1000))
+                   
+        sum += value
     }
-    return sum/length
+    console.log(100-sum/(length*10))
+    return 100-sum/(length*10)
 }
 
 exports.getVector = async(url) => {    
@@ -41,19 +42,20 @@ exports.getVector = async(url) => {
     const fileList = await getImages(identifier)
 
     const net = await posnet.load({
-        architecture: 'ResNet50',
-        outputStride: 32,
-        inputResolution: { width: 257, height: 200 },
-        quantBytes: 2});
-    const canvas = createCanvas(500,500)
-    const ctx = canvas.getContext('2d')    
+        architecture: 'MobileNetV1',
+        outputStride: 16,
+        inputResolution: { width: 400, height: 300 },
+        multiplier: 0.75});   
 
-    for await(const fileName of fileList){
+        const canvas = createCanvas(width,height)
+        const ctx = canvas.getContext('2d')   
+
+        for await(const fileName of fileList){
         const img = new Image();
         img.onload = () => ctx.drawImage(img,0,0);
         img.src = path.join(__dirname,`../images/${fileName}`)
         const input = tf.browser.fromPixels(canvas)
-        const pose = await net.estimateSinglePose(input,imageScaleFactor,flipHorizontal,outputStride)
+        const pose = await net.estimateSinglePose(input)
         pose.keypoints = pose.keypoints.filter(it => !exclude.includes(it.part))
         output.push(pose)
     }
